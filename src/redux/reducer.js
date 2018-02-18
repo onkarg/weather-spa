5abcd1446aab9d15f96fdbbb22997048650a3650
Onkar <onkarg@gmail.com>
2018-02-17 18:08:00 -0700

    redux(reducer, store) setup w/ api key, api url

---
+++ src/redux/reducer.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+const initialState = {
+    weather: [],
+    forecast: []
+}
+
+const API_KEY = '8b23d49b8a98da201482ad7f9e6dba8b';
+
+const WEATHER_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+const FORECAST_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+
+const FETCH_WEATHER = "FETCH_WEATHER";
+const FETCH_FORECAST = "FETCH_FORECAST";
+
+export function fetchWeather (city){
+    const request = axios.get(`${WEATHER_URL}&q=${city},us`).then(response => {
+        return response.data;
+    })
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
+
+export function fetchForecast (city){
+    const request = axios.get(`${FORECAST_URL}&q=${city},us`).then(response => {
+        return response.data;
+    })
+    return {
+        type: FETCH_FORECAST,
+        payload: request
+    };
+}
+
+export default function reducer(state = [], action){
+    switch(action.type){
+        case FETCH_WEATHER + "_FULFILLED":
+            return Object.assign({}, state, {weather: action.payload});
+        default: 
+            return state; 
+    }
+}
