48e2160267c67e9e32ed8a77c7bf85023bacf7f1
Onkar Goswami <onkarg@gmail.com>
2018-02-22 00:09:32 -0700

    added cities now change current weather

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 import { Provider } from "react-redux";
-import configureStore from "./store";
-import { fetchLocation } from './actions';
+import createAppStore from './store'
+import { fetchLocation } from "./actions";
+import { PersistGate } from "redux-persist/integration/react";
 
-const store = configureStore();
+const { persistor, store } = createAppStore()
 
 store.dispatch(fetchLocation());
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,40 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers";
 import thunkMiddleware from "redux-thunk";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default function configureStore(initialState) {
-  const store = createStore(
-    rootReducer,
-    initialState,
-    composeEnhancers(applyMiddleware(thunkMiddleware))
-  );
-  return store;
+const configureStore = composeEnhancers(
+  applyMiddleware(thunkMiddleware),
+)(createStore)
+
+const persistConfig = {
+  key: 'root',
+  storage,
 }
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const createAppStore = () => {
+  let store = configureStore(persistedReducer)
+  let persistor = persistStore(store)
+
+  return { persistor, store }
+}
+
+export default createAppStore
+
+
+
+
+
+// export default function configureStore(initialState) {
+//   const store = createStore(
+//     rootReducer,
+//     initialState,
+//     composeEnhancers(applyMiddleware(thunkMiddleware))
+//   );
+//   return store;
+// }
